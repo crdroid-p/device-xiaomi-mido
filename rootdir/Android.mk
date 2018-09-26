ef19579f8cda2b4ffd3d9321293b50154172e44d
relikvent-som <39597547+relikvent-som@users.noreply.github.com>
2018-09-26 08:55:35 +0530

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 # Device Script
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mido.rc
+LOCAL_MODULE       := init.oxygen.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.mido.rc
